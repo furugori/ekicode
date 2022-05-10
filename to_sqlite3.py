f3bf79a318eba9a4122e5a7de3789e3e0fe14cda
HideoFURUGORI <68085615+HideoFURUGORI@users.noreply.github.com>
2022-05-10 16:11:04 +0900

    変更
    カラム名変更．dbの路線コード，駅順コードのゼロパディング

--- to_sqlite3.py
+++ to_sqlite3.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 # csvテーブルのインポート
 df = pd.read_csv('ekicode.csv')
+# 路線コードと駅順コードのゼロパディング
+zf2 = lambda s: s.zfill(2)
+df['路線コード'] = df['路線コード'].map(zf2)
+df['路線コード'] = df['路線コード'].map(zf2)
+df['駅順コード'] = df['駅順コード'].map(zf2)
+df['駅順コード'] = df['駅順コード'].map(zf2)
 # ダンプ
 with sqlite3.connect('ekicode.sqlite3', isolation_level=None) as conn:
     df.to_sql(name='駅コード', con=conn)
